7c824335ece91eedc5ec37833f0929feea1cc506
Farouk12385 <ahmedfarook229@gmail.com>
2025-04-30 22:45:56 +0300

    Update Student.java

--- Student.java
+++ Student.java
@@ -1,35 +1,55 @@
-public class Student {
-    // Instance variables
-    // studentID is the ID of the student
-    // nextStudent is a reference to the next student in the list
-    // It is null if there is no next student
-int studentID;// ID of the student
-Student nextStudent;// reference to the next student
-public Student() {
-}
-public Student(int StudentID) {
-    // Initialize the StudentID
-    this.studentID = StudentID;
-}
-public Student (int studentID, Student nextStudent) {
-    // Initialize the StudentID
-    // and set nextStudent to the given value
-    this.studentID = studentID;
-    this.nextStudent = nextStudent;
-}
-public void addcourse(int course) {
-    // Add a course to the student
-    if (this.nextStudent == null) {
-        this.nextStudent = new Student(course);
-    } else {
-        this.nextStudent.addcourse(course);
+class Student {
+    int studentID;
+    int courseCount;
+    Student nextStudent;
+    int[] enrolledCourses = new int[7]; // Assuming a student can enroll in up to 7 courses
+
+    public Student(int studentID) {
+        this.studentID = studentID;
+        this.courseCount = 0;
+        this.nextStudent = null;
+        
+
     }
 
-}
-    // Method to check if the student is enrolled in a  course
-    public boolean isEnrolledInCourse(int courseID) {
 
+    
+    public Student(int studentID, Student nextStudent) {
+        this.studentID = studentID;
+        this.courseCount = 0;
+        this.nextStudent = nextStudent;
+
+
+    }
+
+
+
+    public boolean isEnrolledInCourse(int courseID) {
+        for (int i = 0; i < courseCount; i++) {
+            if (enrolledCourses[i] == courseID) {
+                return true;
+            }
+        }
         return false;
     }
 
+    public void addCourse(int courseID) {
+        if (courseCount < 7) {
+            enrolledCourses[courseCount++] = courseID;
+        }
+    }
+
+   public void removeCourse(int courseID) {
+        for (int i = 0; i < courseCount; i++) {
+            if (enrolledCourses[i] == courseID) {
+                // Shift remaining courses to fill the gap
+                for (int j = i; j < courseCount - 1; j++) {
+                    enrolledCourses[j] = enrolledCourses[j + 1];
+                }
+                courseCount--;
+                break;
+            }
+        }
+
+    }
 }
